ff19826fafd544bc9afbaa197abaccd956b0c1ae
Chad Green <chadgreen@chadgreen.com>
2020-10-28 13:29:15 -0400

    Built the tutorial as documented at https://docs.microsoft.com/en-us/azure/cosmos-db/sql-api-dotnet-application

---
+++ Demos/Tutorial/Tutorial/Services/CosmosDbService.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Azure.Cosmos;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tutorial.Models;
+
+namespace Tutorial.Services
+{
+
+	public class CosmosDbService : ICosmosDbService
+	{
+
+		private Container _container;
+
+		public CosmosDbService(CosmosClient cosmosClient, string databaseName, string containerName)
+		{
+			_container = cosmosClient.GetContainer(databaseName, containerName);
+		}
+
+		public async Task AddItemAsync(Item item)
+		{
+			await _container.CreateItemAsync<Item>(item, new PartitionKey(item.Id));
+		}
+
+		public async Task DeleteItemAsync(string id)
+		{
+			await _container.DeleteItemAsync<Item>(id, new PartitionKey(id));
+		}
+
+		public async Task<Item> GetItemAsync(string id)
+		{
+			try
+			{
+				ItemResponse<Item> response = await _container.ReadItemAsync<Item>(id, new PartitionKey(id));
+				return response.Resource;
+			}
+			catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+			{
+				return null;
+			}
+		}
+
+		public async Task<IEnumerable<Item>> GetItemsAsync(string queryString)
+		{
+			var query = _container.GetItemQueryIterator<Item>(new QueryDefinition(queryString));
+			List<Item> results = new List<Item>();
+			while (query.HasMoreResults)
+			{
+				var response = await query.ReadNextAsync();
+				results.AddRange(response.ToList());
+			}
+			return results;
+		}
+
+		public async Task UpdateItemAsync(string id, Item item)
+		{
+			await _container.UpsertItemAsync<Item>(item, new PartitionKey(id));
+		}
+
+	}
+
+}
---
+++ Demos/Tutorial/Tutorial/Services/ICosmosDbService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tutorial.Models;
+
+namespace Tutorial.Services
+{
+	public interface ICosmosDbService
+	{
+		Task AddItemAsync(Item item);
+		Task DeleteItemAsync(string id);
+		Task<Item> GetItemAsync(string id);
+		Task<IEnumerable<Item>> GetItemsAsync(string queryString);
+		Task UpdateItemAsync(string id, Item item);
+	}
+}
