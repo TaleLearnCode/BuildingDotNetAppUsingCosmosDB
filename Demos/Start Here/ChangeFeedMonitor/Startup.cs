40521817735be61f34b664179f78409b3e382e36
Chad Green <chadgreen@chadgreen.com>
2020-11-08 23:06:06 -0500

    Continued development of the slides and demo code

---
+++ Demos/Start Here/ChangeFeedMonitor/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using TaleLearnCode.Todo.Services;
+
+[assembly: FunctionsStartup(typeof(TaleLearnCode.Todo.Functions.Startup))]
+namespace TaleLearnCode.Todo.Functions
+{
+
+	/// <summary>
+	/// Sets up everything for processing.
+	/// </summary>
+	/// <seealso cref="FunctionsStartup" />
+	public class Startup : FunctionsStartup
+	{
+
+		/// <summary>
+		/// Configures the specified builder.
+		/// </summary>
+		/// <param name="builder">The builder.</param>
+		public override void Configure(IFunctionsHostBuilder builder)
+		{
+			CosmosClient cosmosClient = new CosmosClient(Environment.GetEnvironmentVariable("CosmosConnectionString"));
+			builder.Services.AddSingleton<IMetadataService>((s) => { return new MetadataService(cosmosClient, Settings.DatabaseName, Settings.MetadataContainerName, Environment.GetEnvironmentVariable("AzureCacheConnectionString")); });
+			builder.Services.AddSingleton<ITodoService>(
+				(s) =>
+				{
+					return new TodoService(
+						cosmosClient,
+						Settings.DatabaseName,
+						Settings.ItemContainerName,
+						Environment.GetEnvironmentVariable("ArchiveStorageAccount"),
+						Environment.GetEnvironmentVariable("ArchiveStorageKey"),
+						Environment.GetEnvironmentVariable("ArchiveContainer"));
+				});
+		}
+
+	}
+
+}
--- Demos/Start Here/Services/Common.cs
+++ Demos/Start Here/Services/Common.cs
@@ -1,95 +1,10 @@
 ﻿using Microsoft.Azure.Cosmos;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace TaleLearnCode.Todo.Services
 {
-
-	///// <summary>
-	///// Provides a set of common Cosmos DB functionality for the rest of the library to use.
-	///// </summary>
-	//internal static class Common
-	//{
-
-	//	/// <summary>
-	//	/// Creates an item in the Cosmos DB database.
-	//	/// </summary>
-	//	/// <typeparam name="T">The type being created in the database.</typeparam>
-	//	/// <param name="container">The <see cref="Container"/> where to save the item.</param>
-	//	/// <param name="item">The item to be saved in the database.</param>
-	//	/// <param name="partitionKey">The partition key of the item being saved.</param>
-	//	/// <returns>The resulting database object.</returns>
-	//	internal static async Task<T> CreateItemAsync<T>(Container container, T item, string partitionKey)
-	//	{
-	//		throw new NotImplementedException();
-	//	}
-
-	//	/// <summary>
-	//	/// Deletes the item from the Cosmos DB database.
-	//	/// </summary>
-	//	/// <typeparam name="T">The type being deleted from the database.</typeparam>
-	//	/// <param name="container">The <see cref="Container"/> where to delete the item.</param>
-	//	/// <param name="id">Identifier of the item to be deleted.</param>
-	//	/// <param name="partitionKey">The partition key of the item to be deleted.</param>
-	//	internal static async Task DeleteItemAsync<T>(Container container, string id, string partitionKey)
-	//	{
-	//		throw new NotImplementedException();
-	//	}
-
-	//	/// <summary>
-	//	/// Updates the specified item in the Cosmos DB database.
-	//	/// </summary>
-	//	/// <typeparam name="T">The type to be updated.</typeparam>
-	//	/// <param name="container">The <see cref="Container"/> where to update the item.</param>
-	//	/// <param name="partitionKey">The partition key of the item to be updated.</param>
-	//	/// <param name="item">The item to be updated.</param>
-	//	internal static async Task UpdateItemAsync<T>(Container container, string partitionKey, T item)
-	//	{
-	//		throw new NotImplementedException();
-	//	}
-
-	//	/// <summary>
-	//	/// Gets the item from the Cosmos DB database by its identifier.
-	//	/// </summary>
-	//	/// <typeparam name="T">The type to be retrieved.</typeparam>
-	//	/// <param name="container">The <see cref="Container"/> where to retrieve the item.</param>
-	//	/// <param name="id">Identifier of the item to be retrieved.</param>
-	//	/// <param name="partitionKey">The partition key of the item to be retrieved.</param>
-	//	/// <returns>The requested item from the database.</returns>
-	//	internal static async Task<T> GetItemAsync<T>(Container container, string id, string partitionKey)
-	//	{
-	//		throw new NotImplementedException();
-	//	}
-
-	//	/// <summary>
-	//	/// Executes the specified query against the database.
-	//	/// </summary>
-	//	/// <typeparam name="T">The type to be queried against.</typeparam>
-	//	/// <param name="container">The <see cref="Container"/> where to execute the query.</param>
-	//	/// <param name="queryString">The query string to be executed against the database.</param>
-	//	/// <returns>A <see cref="IEnumerable{T}"/> representing the results of the supplied query.</returns>
-	//	internal static async Task<IEnumerable<T>> ExecuteQueryAsync<T>(Container container, string queryString)
-	//	{
-	//		throw new NotImplementedException();
-	//	}
-
-	//	/// <summary>
-	//	/// Executes the specified query.
-	//	/// </summary>
-	//	/// <typeparam name="T">The type to be queried against.</typeparam>
-	//	/// <param name="container">The <see cref="Container"/> where to execute the query.</param>
-	//	/// <param name="queryDefinition">The <see cref="QueryDefinition"/> to be executed.</param>
-	//	/// <returns>A <see cref="IEnumerable{T}"/> representing the results of the supplied query.</returns>
-	//	internal static async Task<IEnumerable<T>> ExecuteQueryAsync<T>(Container container, QueryDefinition queryDefinition)
-	//	{
-	//		throw new NotImplementedException();
-	//	}
-
-
-	//}
-
 
 	/// <summary>
 	/// Provides a set of common Cosmos DB functionality for the rest of the library to use.
@@ -107,8 +22,7 @@ namespace TaleLearnCode.Todo.Services
 		/// <returns>The resulting database object.</returns>
 		internal static async Task<T> CreateItemAsync<T>(Container container, T item, string partitionKey)
 		{
-			ItemResponse<T> itemResponse = await container.CreateItemAsync<T>(item, new PartitionKey(partitionKey));
-			return itemResponse.Resource;
+			throw new NotImplementedException();
 		}
 
 		/// <summary>
@@ -120,7 +34,7 @@ namespace TaleLearnCode.Todo.Services
 		/// <param name="partitionKey">The partition key of the item to be deleted.</param>
 		internal static async Task DeleteItemAsync<T>(Container container, string id, string partitionKey)
 		{
-			await container.DeleteItemAsync<T>(id, new PartitionKey(partitionKey));
+			throw new NotImplementedException();
 		}
 
 		/// <summary>
@@ -132,7 +46,7 @@ namespace TaleLearnCode.Todo.Services
 		/// <param name="item">The item to be updated.</param>
 		internal static async Task UpdateItemAsync<T>(Container container, string partitionKey, T item)
 		{
-			await container.UpsertItemAsync<T>(item, new PartitionKey(partitionKey));
+			throw new NotImplementedException();
 		}
 
 		/// <summary>
@@ -145,15 +59,7 @@ namespace TaleLearnCode.Todo.Services
 		/// <returns>The requested item from the database.</returns>
 		internal static async Task<T> GetItemAsync<T>(Container container, string id, string partitionKey)
 		{
-			try
-			{
-				ItemResponse<T> response = await container.ReadItemAsync<T>(id, new PartitionKey(partitionKey));
-				return response.Resource;
-			}
-			catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
-			{
-				return default;
-			}
+			throw new NotImplementedException();
 		}
 
 		/// <summary>
@@ -165,7 +71,7 @@ namespace TaleLearnCode.Todo.Services
 		/// <returns>A <see cref="IEnumerable{T}"/> representing the results of the supplied query.</returns>
 		internal static async Task<IEnumerable<T>> ExecuteQueryAsync<T>(Container container, string queryString)
 		{
-			return await ExecuteQueryAsync<T>(container, new QueryDefinition(queryString));
+			throw new NotImplementedException();
 		}
 
 		/// <summary>
@@ -177,12 +83,9 @@ namespace TaleLearnCode.Todo.Services
 		/// <returns>A <see cref="IEnumerable{T}"/> representing the results of the supplied query.</returns>
 		internal static async Task<IEnumerable<T>> ExecuteQueryAsync<T>(Container container, QueryDefinition queryDefinition)
 		{
-			var query = container.GetItemQueryIterator<T>(queryDefinition);
-			List<T> results = new List<T>();
-			while (query.HasMoreResults)
-				results.AddRange((await query.ReadNextAsync()).ToList());
-			return results;
+			throw new NotImplementedException();
 		}
+
 
 	}
 
